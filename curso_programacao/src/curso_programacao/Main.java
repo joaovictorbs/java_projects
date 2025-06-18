06bf24f951277565c3aa639731af07e92e2828b6
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2025-06-18 19:59:31 -0300

    Processamento de dados em Java, Casting

--- curso_programacao/src/curso_programacao/Main.java
+++ curso_programacao/src/curso_programacao/Main.java
@@ -3,26 +3,31 @@ import java.util.Locale;
 
 public class Main {
 	public static void main(String[] args) {
-		String product1 = "Computer";
-		String product2 = "Office desk";
+
+		double a, b, B, h, area, c;
+		int A, x, y, z;
 		
-		int age = 30;
-		int code = 5290;
-		char gender = 'F';
+		a = 5.0;
+		A = (int) a;
+	
+		x = 5;
+		y = 2 * x;
 		
-		double price1 = 2100.0;
-		double price2 = 650.50;
-		double measure = 53.234567;
+		z = 5;
+		c = 2 * z;
 		
-		System.out.println("Products:");
-		System.out.printf("%s, whice price is $ %.2f%n", product1, price1);
-		System.out.printf("%s, whice price is $ %.2f%n%n", product2, price2);
+		b = 6.0;
+		B = 8.0;
+		h = 5.0;
 		
-		System.out.println("Record: " + age + " years old, code " + code + " and gender: " + gender + "\n");
+		area = (b + B) / 2.0 * h;
 		
-		System.out.printf("Measue with eight decimal places: %.8f%n", measure);
-		System.out.printf("Rouded (three decimal places): %.3f%n", measure);
-		Locale.setDefault(Locale.US);
-		System.out.printf("US decimal point: %.3f%n", measure);
+		System.out.println(A);
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+		System.out.println(c);
+		System.out.println(area);
+		
 	}
 }
