1527896b44f957dcf36a03b8fb626a086ab29617
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2025-06-21 01:02:59 -0300

    Entrada de dados em Java - 2

--- curso_programacao/src/curso_programacao/Main.java
+++ curso_programacao/src/curso_programacao/Main.java
@@ -5,23 +5,22 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 
-		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		String x;
-		int y;
-		double z;
-		char a;
+		int x;
+		String s1, s2, s3;
 		
-		x = sc.next();
-		y = sc.nextInt();
-		z = sc.nextDouble();
-		a = sc.next().charAt(0);
+		x = sc.nextInt();
+		sc.nextLine();
+		s1 = sc.nextLine();
+		s2 = sc.nextLine();
+		s3 = sc.nextLine();
 		
-		System.out.println("Voce digitou: " + x);
-		System.out.println("Voce digitou: " + y);
-		System.out.println("Voce digitou: " + z);
-		System.out.println("Voce digitou: " + a);
+		System.out.println("DADOS DIGITADOS:");
+		System.out.println(x);
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(s3);
 		
 		sc.close();
 		
