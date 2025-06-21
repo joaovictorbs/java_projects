44677a2f25cfb50dd272af2745d6f666a60721a4
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2025-06-21 01:10:21 -0300

    Funcoes matematicas em Java

--- curso_programacao/src/curso_programacao/Main.java
+++ curso_programacao/src/curso_programacao/Main.java
@@ -5,24 +5,29 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 
-		Scanner sc = new Scanner(System.in);
+		double x = 3.0;
+		double y = 4.0;
+		double z = -5.0;
+		double A, B, C;
 		
-		int x;
-		String s1, s2, s3;
+		A = Math.sqrt(x);
+		B = Math.sqrt(y);
+		C = Math.sqrt(25.0);
+		System.out.println("Raiz quadrada de " + x + " = " + A);
+		System.out.println("Raiz quadrada de " + y + " = " + B);
+		System.out.println("Raiz quadrada de 25 = " + C);
 		
-		x = sc.nextInt();
-		sc.nextLine();
-		s1 = sc.nextLine();
-		s2 = sc.nextLine();
-		s3 = sc.nextLine();
+		A = Math.pow(x, y);
+		B = Math.pow(x, 2.0);
+		C = Math.pow(5.0, 2.0);
+		System.out.println(x + " elevado a " + y + " = " + A);
+		System.out.println(x + " elevado ao quadrado = " + B);
+		System.out.println(x + " 5 elevado ao quadrado " + C);
 		
-		System.out.println("DADOS DIGITADOS:");
-		System.out.println(x);
-		System.out.println(s1);
-		System.out.println(s2);
-		System.out.println(s3);
-		
-		sc.close();
+		A = Math.abs(y);
+		B = Math.abs(z);
+		System.out.println("Valor absoluto de " + y + " = " + A);
+		System.out.println("Valor absoluto de " + z + " = " + B);
 		
 	}
 }
