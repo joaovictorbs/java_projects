1e98694bd53dbbc7a99bce37e5fb9decc2b221f7
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2025-07-04 18:59:36 -0300

    Sintaxe opcional - switch-case

--- curso_programacao/src/curso_programacao/Main.java
+++ curso_programacao/src/curso_programacao/Main.java
@@ -1,22 +1,42 @@
 package curso_programacao;
-import java.util.Locale;
-import java.util.Scanner;
+]import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
 
-		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		int minutos = sc.nextInt();
+		int x = sc.nextInt();
+		String dia;
 		
-		double conta = 50.0;
-		if (minutos > 100) {
-			conta += (minutos - 100) * 2.0;
+		switch(x) {
+		case 1:
+			dia = "domingo";
+			break;
+		case 2:
+			dia = "segunda";
+			break;
+		case 3:
+			dia = "terca";
+			break;
+		case 4:
+			dia = "quarta";
+			break;
+		case 5:
+			dia = "quinta";
+			break;
+		case 6:
+			dia = "sexta";
+			break;
+		case 7:
+			dia = "Sabado";
+			break;
+		default:
+			dia = "Valor invalido";
+			break;
 		}
 		
-		System.out.printf("Valor da conta = R$ %.2f%n", conta);
-		
+		System.out.println("Dia da semana: " + dia);
 		sc.close();
 	}
 }
