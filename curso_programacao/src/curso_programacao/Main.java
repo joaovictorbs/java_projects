411021f1577af68ee32af739c33974afd75889a3
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2025-07-15 01:01:07 -0300

    Escopo e inicializacao

--- curso_programacao/src/curso_programacao/Main.java
+++ curso_programacao/src/curso_programacao/Main.java
@@ -5,13 +5,18 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 
-		Scanner sc = new Scanner(System.in);
+		double price = 400.00;
 		
-		double preco = 34.5;
-		double desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
-
-		System.out.println(desconto);
+		double discount;
 		
-		sc.close();
+		if (price < 200.00) {
+			discount = price * 0.1;
+		}
+		else {
+			discount = 0;
+		}
+		
+		System.out.println(discount);
+		
 	}
 }
