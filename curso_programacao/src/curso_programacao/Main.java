35b3168e91d5b5d84ddc26a9458611221458d29f
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2025-07-15 23:58:20 -0300

    Estrutura repetitiva enquanto (while)

--- curso_programacao/src/curso_programacao/Main.java
+++ curso_programacao/src/curso_programacao/Main.java
@@ -4,19 +4,18 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-
-		 Locale.setDefault(Locale.US);
 		 
 		 Scanner sc = new Scanner(System.in);
+
+		 int x = sc.nextInt();
 		 
-		 double largura = sc.nextDouble();
-		 double comprimento = sc.nextDouble();
-		 double metroQuadrado = sc.nextDouble();
-		 double area = largura * comprimento;
-		 double preco = area * metroQuadrado;
+		 int soma = 0;
+		 while(x != 0) {
+			 soma += x;
+			 x = sc.nextInt();
+		 }
 		 
-		 System.out.printf("AREA = %.2f%n", area);
-		 System.out.printf("PRECO = %.2f%n", preco);
+		 System.out.println(soma);
 		 
 		 sc.close();
 	}
