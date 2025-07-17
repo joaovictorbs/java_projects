40ba7a823fbb2ca65444fd60a2a1826a1a24e65d
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2025-07-17 01:32:51 -0300

    Exercicios sobre estrutura repetitiva while

--- curso_programacao/src/curso_programacao/Main.java
+++ curso_programacao/src/curso_programacao/Main.java
@@ -5,18 +5,71 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		 
-		 Scanner sc = new Scanner(System.in);
+		/* Exercicio 01 
+		Scanner sc = new Scanner(System.in);
 
-		 int x = sc.nextInt();
+		int senha = sc.nextInt();
 		 
-		 int soma = 0;
-		 while(x != 0) {
-			 soma += x;
-			 x = sc.nextInt();
-		 }
+		while(senha != 2002) {
+			System.out.println("Senha Invalida" + "\n");
+			senha = sc.nextInt();
+		}
 		 
-		 System.out.println(soma);
+		System.out.print("Acesso Permitido");
 		 
-		 sc.close();
+		sc.close();
+		*/
+		
+		/* Exercicio 02
+		Scanner sc = new Scanner(System.in);
+		
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+				
+		while(x != 0 && y != 0) {
+			if(x > 0 && y > 0) {
+				System.out.println("Primeiro" + "\n");
+			}
+			else if(x < 0 && y > 0) {
+				System.out.println("Segundo" + "\n");
+			}
+			else if(x < 0 && y < 0) {
+				System.out.println("Terceiro" + "\n");
+			}
+			else {
+				System.out.println("Quarto" + "\n");
+			}
+			x = sc.nextInt();
+			y = sc.nextInt();
+		}
+		
+		sc.close();
+		*/
+		
+		/* Exercicio 03
+		Scanner sc = new Scanner(System.in);
+		
+		int codCliente, totalAlcool = 0, totalGasolina = 0, totalDiesel = 0;
+		
+		codCliente = sc.nextInt();
+		
+		while(codCliente != 4) {
+			if(codCliente == 1) {
+				totalAlcool += 1;
+			}
+			else if(codCliente == 2) {
+				totalGasolina += 1;
+			}
+			else if(codCliente == 3) {
+				totalDiesel+= 1;
+			}
+			
+			codCliente = sc.nextInt();
+		}
+		
+		System.out.printf("MUITO OBRIGADO %nAlcool: %d%nGasolina: %d%nDiesel: %d", totalAlcool, totalGasolina, totalDiesel);
+		
+		sc.close();
+		*/
 	}
 }
