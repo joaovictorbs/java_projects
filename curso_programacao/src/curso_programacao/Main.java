788b8be9cd9de61f67f132356b2d8ac00f84a267
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2025-07-23 01:08:43 -0300

    Funcoes (sintaxe)

--- curso_programacao/src/curso_programacao/Main.java
+++ curso_programacao/src/curso_programacao/Main.java
@@ -1,26 +1,40 @@
 package curso_programacao;
-import java.util.Locale;
+
 import java.util.Scanner;
-
-/*
-Este programa calcula as raízes de uma equação do segundo grau
-
-Os valores dos coeficientes devem ser digitados um por linha
-*/
 
 public class Main {
 	public static void main(String[] args) {
 		
-		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		double a, b, c, delta;
+		System.out.println("Enter three numbers: ");
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
 		
-		System.out.println("Digite os valores dos coeficientes:");
-		a = sc.nextDouble();
-		b = sc.nextDouble();
-		c = sc.nextDouble();
+		int higher = max(a, b, c);
 		
-		delta = b * b - 4 * a * c; // cálculo do valor de delta
+		showResult(higher);
+		
+		sc.close();
+	}
+	
+	public static int max(int x, int y, int z) {
+		int aux;
+		if (x > y && x > z) {
+			aux = x;
+		}
+		else if (y > z) {
+			aux = y;
+		}
+		else {
+			aux = z;
+		}
+		
+		return aux;
+	}
+	
+	public static void showResult(int value) {
+		System.out.println("Higher = " + value);
 	}
 }
