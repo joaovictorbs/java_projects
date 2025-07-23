4b5dc4d315992881bee4f664b3dd7217012770cd
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2025-07-23 00:46:04 -0300

    Comentarios em Java (basico)

--- curso_programacao/src/curso_programacao/Main.java
+++ curso_programacao/src/curso_programacao/Main.java
@@ -2,61 +2,25 @@ package curso_programacao;
 import java.util.Locale;
 import java.util.Scanner;
 
+/*
+Este programa calcula as raízes de uma equação do segundo grau
+
+Os valores dos coeficientes devem ser digitados um por linha
+*/
+
 public class Main {
 	public static void main(String[] args) {
-		 
-		String original = "abcde FGHIJ ABC abc DEFG     ";
 		
-		String s01 = original.toLowerCase();
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
 		
-		String s02 = original.toUpperCase();
+		double a, b, c, delta;
 		
-		String s03 = original.trim();
+		System.out.println("Digite os valores dos coeficientes:");
+		a = sc.nextDouble();
+		b = sc.nextDouble();
+		c = sc.nextDouble();
 		
-		String s04 = original.substring(2);
-		
-		String s05 = original.substring(2, 9);
-		
-		String s06 = original.replace('a', 'x');
-		
-		String s07 = original.replace("abc", "xy");
-		
-		int i = original.indexOf("bc");
-		
-		int j = original.lastIndexOf("bc");
-		
-		String s = "potato apple lemon orange";
-		
-		String[] vect = s.split(" ");
-
-		
-		
-		System.out.println("Original: -" + original + "-");
-		
-		System.out.println("toLowerCase: -" + s01 + "-");
-		
-		System.out.println("toUpperCase: -" + s02+ "-");
-		
-		System.out.println("trim: -" + s03+ "-");
-		
-		System.out.println("substring(2): -" + s04+ "-");
-		
-		System.out.println("substring(2, 9): -" + s05+ "-");
-		
-		System.out.println("replace('a', 'x'): -" + s06+ "-");
-		
-		System.out.println("replace('abc', 'xy'): -" + s07+ "-");
-		
-		System.out.println("Index of 'bc': " + i);
-		
-		System.out.println("Last index of 'bc': " + j);
-		
-		System.out.println(vect[0]);
-		
-		System.out.println(vect[1]);
-		
-		System.out.println(vect[2]);
-		
-		System.out.println(vect[3]);
+		delta = b * b - 4 * a * c; // cálculo do valor de delta
 	}
 }
