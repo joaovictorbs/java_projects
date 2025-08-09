cb1d1e08b232a1b303cd743823df595808abcc81
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2025-08-09 03:06:19 -0300

    comecando a resolver um segundo problema exemplo

--- curso_programacao/src/entities/Product.java
+++ curso_programacao/src/entities/Product.java
@@ -1,19 +1,19 @@
 package entities;
 
 public class Product {
-	public double name;
+	public String name;
 	public double price;
-	public double quantity;
+	public int quantity;
 	
 	public double totalValueInStock() {
-		
+		return this.price * this.quantity;
 	}
 
 	public void addProducts(int quantity) {
-		
+		this.quantity += quantity;
 	}
 	
 	public void removeProducts(int quantity) {
-		
+		this.quantity -= quantity;
 	}
 }
--- curso_programacao/src/entities/Triangle.java
+++ curso_programacao/src/entities/Triangle.java
@@ -7,6 +7,6 @@
 	 
 	 public double area() {
 		 double p = (a + b + c) / 2.0;
-		 return Math.sqrt(p * (p- a) * (p- b) * (p- c));
+		 return Math.sqrt(p * (p - a) * (p - b) * (p - c));
 	 }
  }
