0ecca1cbff262e65819c509cdd780302101018bd
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2025-08-09 02:49:10 -0300

    Ajuste pastas

---
+++ curso_programacao/src/entities/Product.java
@@ -0,0 +1,19 @@
+package entities;
+
+public class Product {
+	public double name;
+	public double price;
+	public double quantity;
+	
+	public double totalValueInStock() {
+		
+	}
+
+	public void addProducts(int quantity) {
+		
+	}
+	
+	public void removeProducts(int quantity) {
+		
+	}
+}
