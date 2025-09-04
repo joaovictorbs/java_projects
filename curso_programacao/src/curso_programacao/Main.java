74b7af6c27ff400588e941d5f8cf49bd47e93ec9
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2025-09-03 22:54:53 -0300

    Finalizando o programa

--- curso_programacao/src/curso_programacao/Main.java
+++ curso_programacao/src/curso_programacao/Main.java
@@ -22,10 +22,25 @@ public class Main {
 		System.out.print("Quantity in stock: ");
 		product.quantity = sc.nextInt();
 				
-		product.toString();
 		
-		//System.out.println(product.name + ", " + product.price + ", " + product.quantity);
-		System.out.println(product); //chama o toString
+		System.out.println();
+		System.out.println("Product data: " + product); //chama o toString
+		
+		System.out.println();
+		System.out.print("Enter the number of products to be added in stock: ");
+		int quantity = sc.nextInt();
+		product.addProducts(quantity);
+		
+		System.out.println();
+		System.out.println("Updated data: " + product);
+		
+		System.out.println();
+		System.out.print("Enter the number of products to be removed from stock: ");
+		quantity = sc.nextInt();
+		product.removeProducts(quantity);
+		
+		System.out.println();
+		System.out.println("Updated data: " + product);
 		
 		sc.close();
 	}
