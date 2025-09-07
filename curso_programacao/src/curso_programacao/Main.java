a3f3f7308e803e4a33646575e490c3ac3ffd9798
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2025-09-07 17:36:11 -0300

    Exercicios de fixacao

--- curso_programacao/src/curso_programacao/Main.java
+++ curso_programacao/src/curso_programacao/Main.java
@@ -1,47 +1,76 @@
 package curso_programacao;
 
 import java.util.Locale;
-
 import java.util.Scanner;
-import entities.Product;
-
+import entities.Rectangle;
+import entities.Employee;
+import entities.Student;
 
 public class Main {
 	public static void main(String[] args) {
 		
+		/*EXERCICIO 1
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-
-		Product product = new Product();
 		
-		System.out.println("Enter product data: ");
-		System.out.print("Name: ");
-		product.name = sc.nextLine();
-		System.out.print("Price: ");
-		product.price = sc.nextDouble();
-		System.out.print("Quantity in stock: ");
-		product.quantity = sc.nextInt();
+		Rectangle rect = new Rectangle();
+		
+		System.out.println("Enter rectangle width and height:");
+		
+		rect.width = sc.nextDouble();
+		rect.height = sc.nextDouble();
+		
+		System.out.printf("AREA = %.2f%n", rect.area());
+		System.out.printf("PERIMETER = %.2f%n", rect.perimeter());
+		System.out.printf("DIAGONAL = %.2f%n", rect.diagonal());
 				
-		
-		System.out.println();
-		System.out.println("Product data: " + product); //chama o toString
-		
-		System.out.println();
-		System.out.print("Enter the number of products to be added in stock: ");
-		int quantity = sc.nextInt();
-		product.addProducts(quantity);
-		
-		System.out.println();
-		System.out.println("Updated data: " + product);
-		
-		System.out.println();
-		System.out.print("Enter the number of products to be removed from stock: ");
-		quantity = sc.nextInt();
-		product.removeProducts(quantity);
-		
-		System.out.println();
-		System.out.println("Updated data: " + product);
-		
 		sc.close();
+		*/
+		
+		/*EXERCICIO 2
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Employee emp = new Employee();
+		
+		System.out.println("Name:");
+		emp.name = sc.nextLine();
+		
+		System.out.println("Gross salary:");
+		emp.grossSalary = sc.nextDouble();
+		
+		System.out.println("Tax:");
+		emp.tax= sc.nextDouble();
+		
+		System.out.println("Employee: " + emp);
+		
+		System.out.println("Which percentage to increase salary?");
+		emp.increaseSalary(sc.nextDouble());
+		
+		System.out.println("Updated data: " + emp);
+		*/
+		
+		/*EXERCICIO 3 //testando exemplo com array
+		Locale.setDefault(Locale.US);
+		
+		Scanner sc = new Scanner(System.in);
+		Student stud = new Student();
+		
+		int trimestre = 0;
+		double[] arrGrades = new double[3];
+		
+		stud.name = sc.nextLine();
+		
+		while(trimestre < 3) {
+			arrGrades[trimestre] = sc.nextDouble();
+			trimestre += 1;
+		}
+		
+		stud.addGrade(arrGrades);
+		stud.calculateGrade();
+		stud.missingPoints();
+
+		System.out.println(stud);
+		*/
 	}
 }
