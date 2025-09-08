f55593c851bed4c87400206ec5289394487c18db
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2025-09-08 14:52:03 -0300

    Membros estaticos - 1

--- curso_programacao/src/curso_programacao/Main.java
+++ curso_programacao/src/curso_programacao/Main.java
@@ -2,75 +2,30 @@ package curso_programacao;
 
 import java.util.Locale;
 import java.util.Scanner;
-import entities.Rectangle;
-import entities.Employee;
-import entities.Student;
+
+import util.Calculator;
 
 public class Main {
+		
 	public static void main(String[] args) {
 		
-		/*EXERCICIO 1
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+		Calculator calc = new Calculator();
 		
-		Rectangle rect = new Rectangle();
+		double circumference;
+		double volume;
 		
-		System.out.println("Enter rectangle width and height:");
+		System.out.println("Enter radius:");
+		double radius = sc.nextDouble();
 		
-		rect.width = sc.nextDouble();
-		rect.height = sc.nextDouble();
+		circumference = calc.circumference(radius);
+		volume = calc.volume(radius);
 		
-		System.out.printf("AREA = %.2f%n", rect.area());
-		System.out.printf("PERIMETER = %.2f%n", rect.perimeter());
-		System.out.printf("DIAGONAL = %.2f%n", rect.diagonal());
-				
+		System.out.printf("Circumference: %.2f%n", circumference);
+		System.out.printf("Volume: %.2f%n", volume);
+		System.out.printf("PI value: %.2f%n", calc.PI);
+		
 		sc.close();
-		*/
-		
-		/*EXERCICIO 2
-		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner(System.in);
-		
-		Employee emp = new Employee();
-		
-		System.out.println("Name:");
-		emp.name = sc.nextLine();
-		
-		System.out.println("Gross salary:");
-		emp.grossSalary = sc.nextDouble();
-		
-		System.out.println("Tax:");
-		emp.tax= sc.nextDouble();
-		
-		System.out.println("Employee: " + emp);
-		
-		System.out.println("Which percentage to increase salary?");
-		emp.increaseSalary(sc.nextDouble());
-		
-		System.out.println("Updated data: " + emp);
-		*/
-		
-		/*EXERCICIO 3 //testando exemplo com array
-		Locale.setDefault(Locale.US);
-		
-		Scanner sc = new Scanner(System.in);
-		Student stud = new Student();
-		
-		int trimestre = 0;
-		double[] arrGrades = new double[3];
-		
-		stud.name = sc.nextLine();
-		
-		while(trimestre < 3) {
-			arrGrades[trimestre] = sc.nextDouble();
-			trimestre += 1;
-		}
-		
-		stud.addGrade(arrGrades);
-		stud.calculateGrade();
-		stud.missingPoints();
-
-		System.out.println(stud);
-		*/
 	}
 }
