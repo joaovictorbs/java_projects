3259ac54e14a61df908c89e8987cf83f63cbd67c
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2025-09-10 02:22:39 -0300

    Vetores - 1

--- curso_programacao/src/curso_programacao/Main.java
+++ curso_programacao/src/curso_programacao/Main.java
@@ -3,7 +3,6 @@ package curso_programacao;
 import java.util.Locale;
 import java.util.Scanner;
 
-import entities.BankAccount;
 
 public class Main {
 		
@@ -12,44 +11,22 @@ public class Main {
 		/*EXERCICIO 1*/
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		double accountValue = 0.0;
+
+		int n = sc.nextInt();
+		double[] vect = new double[n];
 		
-		System.out.println("Enter account number: ");
-		int accountNumber = sc.nextInt();
-		sc.nextLine(); //consome a quebra de linha deixada pelo nextInt
-		
-		System.out.println("Enter account holder: ");
-		String accountHolder = sc.nextLine();
-		
-		System.out.println("Is there an initial deposit (y/n)?");
-		char response = sc.next().charAt(0);
-				
-		if(response == 'y') {
-			System.out.println("Enter initial deposit value: ");
-			accountValue = sc.nextDouble();
+		for(int i = 0; i < n; i++) {
+			vect[i] = sc.nextDouble();
 		}
 		
-		BankAccount bank = new BankAccount(accountNumber, accountHolder, accountValue);
+		double sum = 0.0;
+		for(int i = 0; i<n; i++) {
+			sum += vect[i];
+		}
 		
-		System.out.println();
-		System.out.println(bank);
-		System.out.println();
+		double average = sum / n;
 		
-		System.out.println("Enter a deposit value: ");
-		accountValue = sc.nextDouble();
-		bank.deposit(accountValue);
-		
-		
-		System.out.println();
-		System.out.println(bank);
-		System.out.println();
-		
-		System.out.println("Enter a withdraw value: ");
-		accountValue = sc.nextDouble();
-		bank.withdraw(accountValue);
-		
-		System.out.println();
-		System.out.println(bank);
+		System.out.printf("AVERAGE HEIGHT = %.2f", average);
 		
 		sc.close();
 	}
