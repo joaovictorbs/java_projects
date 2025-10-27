5f3fdb9e8d9bb8e934e9ccda803bda918d31dd38
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2025-10-27 13:23:37 -0300

    Exercicio vetores diagonal

--- curso_programacao/src/curso_programacao/Main.java
+++ curso_programacao/src/curso_programacao/Main.java
@@ -9,93 +9,38 @@ import java.util.stream.Collectors;
 import entities.EmployeeSalary;
 
 public class Main {
-		
-	public static void main(String[] args) {
-		
-		/*EXERCICIO FIXACAO LISTA*/
-		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.print("How many employees will be registered? ");
-		int numberEmployees = sc.nextInt();
-		sc.nextLine();
-		
-		System.out.println();
-		
-		List<EmployeeSalary> employeeSalary = new ArrayList<EmployeeSalary>();
-		
-		for(int i=0; i<numberEmployees; i++) {
-			
-			System.out.println("Employee #" + (i+1));
-			
-			System.out.print("Id: ");
-			int id = sc.nextInt();
-			sc.nextLine();
-			while (hasId(employeeSalary, id)) {
-				System.out.println("Id already taken! Try again: ");
-				id = sc.nextInt();
-				sc.nextLine();
-			}
-			
-			System.out.print("Name: ");
-			String name = sc.nextLine();
-			
-			System.out.print("Salary: ");
-			double salary = sc.nextDouble();
-			sc.nextLine();
-		
-			System.out.println();
-			
-			EmployeeSalary emp = new EmployeeSalary(id, name, salary); 
-			
-			employeeSalary.add(emp); 
-		}
-		
-		System.out.print("Enter the employee id that will have salary increase: ");
-		int idSearch = sc.nextInt();
-		Integer indexPercentage = null;
-	
-		for(int i=0;i<employeeSalary.size();i++) {
-			if(employeeSalary.get(i).getId() == idSearch) {
-				indexPercentage = i;	
-			}
-		}
-		
-		EmployeeSalary emp = employeeSalary.stream()
-			    .filter(e -> e.getId() == idSearch)
-			    .findFirst()
-			    .orElse(null);
-		
-		if(emp != null) {
-			System.out.print("Enter the percentage: ");
-			emp.increaseSalary(sc.nextDouble());
-		}
-		else {
-			System.out.println("This id does not exist!");
-		}
-		
-		System.out.println();
-		
-		System.out.println("List of employees:");
-		
-		for(int i=0; i<employeeSalary.size(); i++) {
-			System.out.println(employeeSalary.get(i));
-		}
-		
-		sc.close();
-	}
-	
-	public static Integer position(List<EmployeeSalary> employeeSalary, int id) {
-		for (int i = 0; i < employeeSalary.size(); i++) {
-			if (employeeSalary.get(i).getId() == id) {
-				return i;
-			}
-		}
-		return null;
-	}
-	
-	public static boolean hasId(List<EmployeeSalary> employeeSalary, int id ) {
-		EmployeeSalary emp = employeeSalary.stream().filter(x -> x.getId() == id).findFirst().orElse(null);
-		return emp != null;	
-	}
+
+    public static void main(String[] args) {
+
+        /*EXERCICIO MATRIZES*/
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[][] mat = new int[n][n]; //matriz com linha e coluna
+
+        for (int i=0; i<mat.length; i++) {
+            for (int j=0; j<mat.length; j++) {
+                mat[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("Main diagonal:");
+        for (int i=0; i<mat.length; i++) {
+            System.out.print(mat[i][i] + " ");
+        }
+        System.out.println();
+
+        int count = 0;
+        for (int i=0; i<mat.length; i++) {
+            for (int j=0; j<mat[i].length; j++) {
+                if (mat[i][j] < 0) {
+                    count++;
+                }
+
+            }
+        }
+        System.out.println("Negative numbers: " + count);
+
+        sc.close();
+    }
 }
