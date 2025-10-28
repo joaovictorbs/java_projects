ca8d5e1434003d307b7d08f18fd371a80d843a5e
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2025-10-28 01:07:16 -0300

    Exercicio fixacao matrizes multidimensionais

--- curso_programacao/src/curso_programacao/Main.java
+++ curso_programacao/src/curso_programacao/Main.java
@@ -1,12 +1,7 @@
 package curso_programacao;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
-import java.util.Scanner;
+import java.util.*;
 import java.util.stream.Collectors;
-
-import entities.EmployeeSalary;
 
 public class Main {
 
@@ -15,31 +10,53 @@ public class Main {
         /*EXERCICIO MATRIZES*/
         Scanner sc = new Scanner(System.in);
 
+        System.out.print("Inform the number of columns: ");
+        int m = sc.nextInt();
+
+        System.out.print("Inform the number of rows in each column: ");
         int n = sc.nextInt();
-        int[][] mat = new int[n][n]; //matriz com linha e coluna
 
-        for (int i=0; i<mat.length; i++) {
-            for (int j=0; j<mat.length; j++) {
-                mat[i][j] = sc.nextInt();
+        int[][] matriz = new int[m][n];
+
+        for (int i=0; i<matriz.length; i++) {
+            System.out.print("Inform the current column row value: ");
+            for (int j=0; j<matriz[i].length; j++) {
+                matriz[i][j] = sc.nextInt();
             }
         }
 
-        System.out.println("Main diagonal:");
-        for (int i=0; i<mat.length; i++) {
-            System.out.print(mat[i][i] + " ");
-        }
-        System.out.println();
+        System.out.print("Inform the number inside vect: ");
+        int x = sc.nextInt();
 
-        int count = 0;
-        for (int i=0; i<mat.length; i++) {
-            for (int j=0; j<mat[i].length; j++) {
-                if (mat[i][j] < 0) {
-                    count++;
+        for (int i=0; i<matriz.length; i++) {
+            for (int j=0; j<matriz[i].length; j++) {
+                if(matriz[i][j] == x) {
+                    String position;
+                    Integer left, right, up, down;
+
+                    //position = Arrays.asList(matriz[i]).indexOf(x);
+                    position = i + "," + j;
+
+                    System.out.println("Position " + position + ":");
+                    if (j != 0) {
+                        left = matriz[i][j-1];
+                        System.out.println("Left: " + left);
+                    }
+                    if (j < n - 1) {
+                        right = matriz[i][j+1];
+                        System.out.println("Right: " + right);
+                    }
+                    if (i != 0) {
+                        up = matriz[i-1][j];
+                        System.out.println("Up: " + up);
+                    }
+                    if (i < m - 1) {
+                        down = matriz[i+1][j];
+                        System.out.println("Down: " + down);
+                    }
                 }
-
             }
         }
-        System.out.println("Negative numbers: " + count);
 
         sc.close();
     }
