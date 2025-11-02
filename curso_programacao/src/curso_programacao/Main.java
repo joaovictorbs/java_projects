98bae26d506f4a9514aad482a22eea9ff7f46b2f
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2025-11-02 16:57:13 -0300

    Instanciando data-hora em Java

--- curso_programacao/src/curso_programacao/Main.java
+++ curso_programacao/src/curso_programacao/Main.java
@@ -1,5 +1,9 @@
 package curso_programacao;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -7,57 +11,37 @@ public class Main {
 
     public static void main(String[] args) {
 
-        /*EXERCICIO MATRIZES*/
-        Scanner sc = new Scanner(System.in);
+        DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
 
-        System.out.print("Inform the number of columns: ");
-        int m = sc.nextInt();
+        LocalDate d01 = LocalDate.now();
+        LocalDateTime d02 = LocalDateTime.now();
+        Instant d03 = Instant.now();
 
-        System.out.print("Inform the number of rows in each column: ");
-        int n = sc.nextInt();
+        LocalDate d04 = LocalDate.parse("2025-10-31");
+        LocalDateTime d05 = LocalDateTime.parse("2025-10-31T03:33:33");
+        Instant d06 = Instant.parse("2025-10-31T03:33:33Z");
 
-        int[][] matriz = new int[m][n];
+        Instant d07 = Instant.parse("2025-10-31T03:33:33-03:00");
 
-        for (int i=0; i<matriz.length; i++) {
-            System.out.print("Inform the current column row value: ");
-            for (int j=0; j<matriz[i].length; j++) {
-                matriz[i][j] = sc.nextInt();
-            }
-        }
+        LocalDate d08 = LocalDate.parse("31/10/2025", fmt1);
+        LocalDateTime d09 = LocalDateTime.parse("31/10/2025 01:30", fmt2);
 
-        System.out.print("Inform the number inside vect: ");
-        int x = sc.nextInt();
+        LocalDate d10 = LocalDate.of(2025, 10, 31);
+        LocalDateTime d11 = LocalDateTime.of(2025, 10, 31, 1, 30);
 
-        for (int i=0; i<matriz.length; i++) {
-            for (int j=0; j<matriz[i].length; j++) {
-                if(matriz[i][j] == x) {
-                    String position;
-                    Integer left, right, up, down;
 
-                    //position = Arrays.asList(matriz[i]).indexOf(x);
-                    position = i + "," + j;
+        System.out.println("d01 = " + d01.toString());
+        System.out.println("d02 = " + d02.toString());
+        System.out.println("d03 = " + d03.toString());
+        System.out.println("d04 = " + d04.toString());
+        System.out.println("d05 = " + d05.toString());
+        System.out.println("d06 = " + d06.toString());
+        System.out.println("d07 = " + d07.toString());
+        System.out.println("d08 = " + d08.toString());
+        System.out.println("d09 = " + d09.toString());
+        System.out.println("d10 = " + d10.toString());
+        System.out.println("d11 = " + d11.toString());
 
-                    System.out.println("Position " + position + ":");
-                    if (j != 0) {
-                        left = matriz[i][j-1];
-                        System.out.println("Left: " + left);
-                    }
-                    if (j < n - 1) {
-                        right = matriz[i][j+1];
-                        System.out.println("Right: " + right);
-                    }
-                    if (i != 0) {
-                        up = matriz[i-1][j];
-                        System.out.println("Up: " + up);
-                    }
-                    if (i < m - 1) {
-                        down = matriz[i+1][j];
-                        System.out.println("Down: " + down);
-                    }
-                }
-            }
-        }
-
-        sc.close();
     }
 }
