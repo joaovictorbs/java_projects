b66d37ac3447c5d3021a08d188e2e6fa98c295f2
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2025-11-03 15:38:24 -0300

    Convertendo data-hora para texto

--- curso_programacao/src/curso_programacao/Main.java
+++ curso_programacao/src/curso_programacao/Main.java
@@ -3,6 +3,7 @@ package curso_programacao;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
 import java.util.stream.Collectors;
@@ -11,37 +12,30 @@ public class Main {
 
     public static void main(String[] args) {
 
-        DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
-
-        LocalDate d01 = LocalDate.now();
-        LocalDateTime d02 = LocalDateTime.now();
-        Instant d03 = Instant.now();
-
         LocalDate d04 = LocalDate.parse("2025-10-31");
         LocalDateTime d05 = LocalDateTime.parse("2025-10-31T03:33:33");
         Instant d06 = Instant.parse("2025-10-31T03:33:33Z");
 
-        Instant d07 = Instant.parse("2025-10-31T03:33:33-03:00");
+        DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+        DateTimeFormatter fmt3 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm").withZone(ZoneId.systemDefault()); //pega fuso horario do usuario
+        DateTimeFormatter fmt4 = DateTimeFormatter.ISO_DATE_TIME;
+        DateTimeFormatter fmt5 = DateTimeFormatter.ISO_INSTANT;
 
-        LocalDate d08 = LocalDate.parse("31/10/2025", fmt1);
-        LocalDateTime d09 = LocalDateTime.parse("31/10/2025 01:30", fmt2);
+        System.out.println("d04 = " + d04.format(fmt1));
+        System.out.println("d04 = " + fmt1.format(d04));
+        System.out.println("d04 = " + d04.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
 
-        LocalDate d10 = LocalDate.of(2025, 10, 31);
-        LocalDateTime d11 = LocalDateTime.of(2025, 10, 31, 1, 30);
+        System.out.println();
 
+        System.out.println("d05 = " + d05.format(fmt1));
+        System.out.println("d05 = " + d05.format(fmt2));
+        System.out.println("d05 = " + d05.format(fmt4));
 
-        System.out.println("d01 = " + d01.toString());
-        System.out.println("d02 = " + d02.toString());
-        System.out.println("d03 = " + d03.toString());
-        System.out.println("d04 = " + d04.toString());
-        System.out.println("d05 = " + d05.toString());
+        System.out.println();
+
+        System.out.println("d06 = " + fmt3.format(d06));
+        System.out.println("d06 = " + fmt5.format(d06));
         System.out.println("d06 = " + d06.toString());
-        System.out.println("d07 = " + d07.toString());
-        System.out.println("d08 = " + d08.toString());
-        System.out.println("d09 = " + d09.toString());
-        System.out.println("d10 = " + d10.toString());
-        System.out.println("d11 = " + d11.toString());
-
     }
 }
