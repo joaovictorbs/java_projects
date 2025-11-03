98240fc832067b1832d455616cea013b113903aa
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2025-11-03 18:25:23 -0300

    Trabalhando com datas - Date - antigo

--- curso_programacao/src/curso_programacao/Main.java
+++ curso_programacao/src/curso_programacao/Main.java
@@ -1,5 +1,7 @@
 package curso_programacao;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
@@ -8,37 +10,38 @@ import java.util.stream.Collectors;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
 
-        LocalDate d04 = LocalDate.parse("2025-10-31");
-        LocalDateTime d05 = LocalDateTime.parse("2025-10-31T03:33:33");
-        Instant d06 = Instant.parse("2025-10-31T03:33:33Z");
-
-        LocalDate pastWeekLocalDate = d04.minusDays(7);
-        LocalDate nextWeekLocalDate = d04.plusDays(7);
-
-        System.out.println("pastWeekLocalDate = " + pastWeekLocalDate);
-        System.out.println("nextWeekLocalDate = " + nextWeekLocalDate);
-
-        LocalDateTime pastWeekLocalDateTime = d05.minusDays(7);
-        LocalDateTime nextWeekLocalDateTime = d05.plusDays(7);
-
-        System.out.println("pastWeekLocalDateTime = " + pastWeekLocalDateTime);
-        System.out.println("nextWeekLocalDateTime = " + nextWeekLocalDateTime);
-
-        Instant pastWeekInstant = d06.minus(7, ChronoUnit.DAYS);
-        Instant nextWeekInstant = d06.plus(7, ChronoUnit.DAYS);
-
-        System.out.println("pastWeekInstant = " + pastWeekInstant);
-        System.out.println("nextWeekInstant = " + nextWeekInstant);
-
-        Duration t1 = Duration.between(pastWeekLocalDate.atStartOfDay(), d04.atStartOfDay()); //nao calcula entre dois localdate / precisa converter
-        Duration t2 = Duration.between(pastWeekLocalDateTime, d05);
-        Duration t3 = Duration.between(pastWeekInstant, d06);
+        SimpleDateFormat sdf1 = new SimpleDateFormat("dd/MM/yyyy");
+        SimpleDateFormat sdf2 = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        SimpleDateFormat sdf3 = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        sdf3.setTimeZone(TimeZone.getTimeZone("GMT"));
 
 
-        System.out.println("t1 dias = " + t1.toDays());
-        System.out.println("t2 dias = " + t2.toDays());
-        System.out.println("t3 dias = " + t3.toDays());
+        Date x1 = new Date();
+        Date x2 = new Date(System.currentTimeMillis());
+        Date x3 = new Date(0L);
+        Date x4 = new Date(1000L * 60L * 60L * 5L);
+
+        Date y1 = sdf1.parse("25/06/2018");
+        Date y2 = sdf2.parse("25/06/2018 15:42:07");
+        Date y3 = Date.from(Instant.parse("2018-06-25T15:42:07Z"));
+
+        System.out.println("----------------------------------");
+        System.out.println("x1: " + sdf2.format(x1));
+        System.out.println("x2: " + sdf2.format(x2));
+        System.out.println("x3: " + sdf2.format(x3));
+        System.out.println("x4: " + sdf2.format(x4));
+        System.out.println("y1: " + sdf2.format(y1));
+        System.out.println("y2: " + sdf2.format(y2));
+        System.out.println("y3: " + sdf2.format(y3));
+        System.out.println("----------------------------------");
+        System.out.println("x1: " + sdf3.format(x1));
+        System.out.println("x2: " + sdf3.format(x2));
+        System.out.println("x3: " + sdf3.format(x3));
+        System.out.println("x4: " + sdf3.format(x4));
+        System.out.println("y1: " + sdf3.format(y1));
+        System.out.println("y2: " + sdf3.format(y2));
+        System.out.println("y3: " + sdf3.format(y3));
     }
 }
