0998ccc9fab466c54e9871bf032c0179f06cd719
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2025-11-03 16:23:50 -0300

    Convertendo data-hora global para local

--- curso_programacao/src/curso_programacao/Main.java
+++ curso_programacao/src/curso_programacao/Main.java
@@ -16,26 +16,28 @@ public class Main {
         LocalDateTime d05 = LocalDateTime.parse("2025-10-31T03:33:33");
         Instant d06 = Instant.parse("2025-10-31T03:33:33Z");
 
-        DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
-        DateTimeFormatter fmt3 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm").withZone(ZoneId.systemDefault()); //pega fuso horario do usuario
-        DateTimeFormatter fmt4 = DateTimeFormatter.ISO_DATE_TIME;
-        DateTimeFormatter fmt5 = DateTimeFormatter.ISO_INSTANT;
+        LocalDate r1 = LocalDate.ofInstant(d06, ZoneId.systemDefault()); //converte instant para data local considerando fuso horario do usuario
+        LocalDate r2 = LocalDate.ofInstant(d06, ZoneId.of("Asia/Tokyo"));
+        LocalDateTime r3 = LocalDateTime.ofInstant(d06, ZoneId.systemDefault());
+        LocalDateTime r4 = LocalDateTime.ofInstant(d06, ZoneId.of("Portugal"));
 
-        System.out.println("d04 = " + d04.format(fmt1));
-        System.out.println("d04 = " + fmt1.format(d04));
-        System.out.println("d04 = " + d04.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+        System.out.println("r1 = " + r1);
+        System.out.println("r2 = " + r2);
+        System.out.println("r3 = " + r3);
+        System.out.println("r4 = " + r4);
 
-        System.out.println();
-
-        System.out.println("d05 = " + d05.format(fmt1));
-        System.out.println("d05 = " + d05.format(fmt2));
-        System.out.println("d05 = " + d05.format(fmt4));
+        System.out.println("d04 dia = " + d04.getDayOfMonth());
+        System.out.println("d04 mês = " + d04.getMonthValue());
+        System.out.println("d04 ano = " + d04.getYear());
 
         System.out.println();
 
-        System.out.println("d06 = " + fmt3.format(d06));
-        System.out.println("d06 = " + fmt5.format(d06));
-        System.out.println("d06 = " + d06.toString());
+        System.out.println("d05 hora = " + d05.getHour());
+        System.out.println("d05 minuto = " + d05.getMinute());
+        /*for (String s : ZoneId.getAvailableZoneIds()) { //zoneids disponiveis
+            System.out.println(s);
+        }*/
+
+
     }
 }
