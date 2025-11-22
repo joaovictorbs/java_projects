84c4c57d8556a6ae4a7a847542a5860f77c454a5
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2025-11-22 17:46:11 -0300

    Exercicio resolvido heranca e polimorfismo

--- curso_programacao/src/entities/Employee.java
+++ curso_programacao/src/entities/Employee.java
@@ -2,20 +2,42 @@ package entities;
 
 public class Employee {
 	public String name;
-	public double grossSalary;
-	public double tax;
-	
-	public double netSalary() {
-		return grossSalary - tax;
-	}
-	
-	public void increaseSalary(double percentage) {
-		grossSalary += grossSalary * percentage / 100.0;
-	}
-	
-	public String toString() {
-		return name
-			+ ", $"
-			+ String.format("%.2f", netSalary());
-	}
-} 
+	public Integer hours;
+	public Double valuePerHour;
+
+    public Employee(){}
+
+    public Employee(String name, Integer hours, Double valuePerHour){
+        this.name = name;
+        this.hours = hours;
+        this.valuePerHour = valuePerHour;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getHours() {
+        return hours;
+    }
+
+    public void setHours(Integer hours) {
+        this.hours = hours;
+    }
+
+    public Double getValuePerHour() {
+        return valuePerHour;
+    }
+
+    public void setValuePerHour(Double valuePerHour) {
+        this.valuePerHour = valuePerHour;
+    }
+
+    public double payment() {
+        return hours * valuePerHour;
+    }
+}
