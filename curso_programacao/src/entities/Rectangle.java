42a31dbd6bc40efc56ccaf8a9d4fe9a8d95dbd58
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2025-11-23 14:30:29 -0300

    Metodos abstratos

--- curso_programacao/src/entities/Rectangle.java
+++ curso_programacao/src/entities/Rectangle.java
@@ -1,19 +1,41 @@
 package entities;
 
-public class Rectangle {
+import entities.enums.Color;
+
+public class Rectangle extends Shape {
 	
-	public double width;
-	public double height;
-	
+	public Double width;
+	public Double height;
+
+    public Rectangle() {
+        super();
+    }
+
+    public Rectangle(Color color, Double width, Double height) {
+        super();
+        this.width = width;
+        this.height = height;
+    }
+
+    public Double getWidth() {
+        return width;
+    }
+
+    public void setWidth(Double width) {
+        this.width = width;
+    }
+
+    public Double getHeight() {
+        return height;
+    }
+
+    public void setHeight(Double height) {
+        this.height = height;
+    }
+
+    @Override
 	public double area() {
-		return width * height;
+        return width * height;
 	}
-	
-	public double perimeter() {
-		return 2 * (width + height);
-	}
-	
-	public double diagonal() {
-		return Math.sqrt(width * width + height * height);
-	}
+
 }
