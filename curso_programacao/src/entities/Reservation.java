4edbf3201290f6bd2efd93d8b8167e0d5abcde54
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2025-11-26 13:08:25 -0300

    Criando reservation para excecoes personalizadas

---
+++ curso_programacao/src/entities/Reservation.java
@@ -0,0 +1,61 @@
+package entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Reservation {
+
+    private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+    private Integer roomNumber;
+    private Date checkIn;
+    private Date checkOut;
+
+    public Reservation(){}
+
+    public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+        this.roomNumber = roomNumber;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    public Integer getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(Integer roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public Date getCheckIn() {
+        return checkIn;
+    }
+
+    public Date getCheckOut() {
+        return checkOut;
+    }
+
+    public long duration() { //long e um inteiro mais longo
+        long diff = checkOut.getTime() - checkIn.getTime(); //diferenca de datas em milisegundos
+        return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS); //converte milisegundos para dias
+    }
+
+    public void updateDate(Date checkIn, Date checkOut) {
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    @Override
+    public String toString() {
+        return "Room "
+                + roomNumber
+                + ", check-in: "
+                + sdf.format(checkIn)
+                + ", check-out: "
+                + sdf.format(checkOut)
+                + ", "
+                + duration()
+                + " nights";
+    }
+}
