8f43285d13fcb9fff31797bde7d1dbc2a62d3e05
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2025-11-28 22:42:12 -0300

    Manipulando pastas com File

--- curso_programacao/src/curso_programacao/Main.java
+++ curso_programacao/src/curso_programacao/Main.java
@@ -1,23 +1,34 @@
 package curso_programacao;
 
-import java.io.*;
+import java.io.File;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        String[] lines = new String[] { "Good morning", "Good afternoon", "Good night"};
+        Scanner sc = new Scanner(System.in);
 
-        String path = "c:\\temp\\out.txt";
+        System.out.println("Enter a folder path: ");
+        String strPath = sc.nextLine();
 
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path))) {
-            for (String line : lines) {
-                bw.write(line);
-                bw.newLine();
-            }
+        File path = new File(strPath);
+
+        File[] folders = path.listFiles(File::isDirectory);
+        System.out.println("FOLDERS: ");
+        for (File folder : folders) {
+            System.out.println(folder);
         }
-        catch (IOException e) {
-            e.printStackTrace();
+
+        File[] files = path.listFiles(File::isFile);
+        System.out.println("FILES:");
+        for (File file : files) {
+            System.out.println(file);
         }
+
+        boolean success = new File(strPath + "\\subdir").mkdir();
+        System.out.println("Directory created successfully: " + success);
+
+        sc.close();
     }
 }
