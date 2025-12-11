e69bcd5d320f179b63bdaa15f7bc4a614db07e67
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2025-12-11 17:40:16 -0300

    Default methods

--- curso_programacao/src/curso_programacao/Main.java
+++ curso_programacao/src/curso_programacao/Main.java
@@ -1,39 +1,28 @@
 package curso_programacao;
 
-import model.entities.ComboDevice;
-import model.entities.ConcretePrinter;
-import model.entities.ConcreteScanner;
-import model.entities.Employee;
+import model.services.BrazilInterestService;
+import model.services.InterestService;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
 
-        List<Employee> list = new ArrayList<>();
-        String path = "C:\\temp\\in.txt";
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
 
-        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+        System.out.print("Amount: ");
+        double amount = sc.nextDouble();
+        System.out.print("Months: ");
+        int months = sc.nextInt();
 
-            String employeeCsv = br.readLine();
-            while (employeeCsv != null) {
-                String[] fields = employeeCsv.split(",");
-                list.add(new Employee(fields[0], Double.parseDouble(fields[1])));
-                employeeCsv = br.readLine();
-            }
-            Collections.sort(list);
-            for (Employee emp : list) {
-                System.out.println(emp.getName() + ", " + emp.getSalary());
-            }
-        } catch (IOException e) {
-            System.out.println("Error: " + e.getMessage());
-        }
+        InterestService is = new BrazilInterestService(2.0);
+        double payment = is.payment(amount, months);
 
+        System.out.println("Payment after " + months + " months:");
+        System.out.println(String.format("%.2f", payment));
+
+        sc.close();
     }
 
 }
