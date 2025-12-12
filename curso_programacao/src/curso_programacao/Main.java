c7b0a1ba77be5f781c05afca345c9b16ea9f843e
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2025-12-12 10:58:44 -0300

    covariancia e contravariancia

--- curso_programacao/src/curso_programacao/Main.java
+++ curso_programacao/src/curso_programacao/Main.java
@@ -1,31 +1,32 @@
 package curso_programacao;
 
-import model.entities.Circle;
-import model.entities.Rectangle;
-import model.entities.Shape;
-
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
 
-        List<Shape> myShapes = new ArrayList<>();
-        myShapes.add(new Rectangle(3.0, 2.0));
-        myShapes.add(new Circle(2.0));
+        List<Integer> myInts = Arrays.asList(1, 2, 3, 4);
+        List<Double> myDoubles = Arrays.asList(3.14, 6.28);
+        List<Object> myObjs = new ArrayList<Object>();
 
-        List<Circle> myCircles = new ArrayList<>();
-        myCircles.add(new Circle(2.0));
-        myCircles.add(new Circle(3.0));
-
-        System.out.println("Total area: " + totalArea(myCircles));
+        copy(myInts, myObjs);
+        printList(myObjs);
+        copy(myDoubles, myObjs);
+        printList(myObjs);
     }
 
-    public static double totalArea(List<? extends Shape> list) { //circle e um sub da shape
-        double sum = 0.0;
-        for (Shape s : list) {
-            sum += s.area();
+    public static void copy(List<? extends Number> source, List<? super Number> destiny) {
+        for(Number number : source) {
+            destiny.add(number);
         }
-        return sum;
+    }
+
+    public static void printList(List<?> list) {
+        for (Object obj : list) {
+            System.out.print(obj + " ");
+        }
+        System.out.println();
+
     }
 
 }
