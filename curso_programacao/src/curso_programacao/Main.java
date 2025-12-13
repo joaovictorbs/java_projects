c11016ff6bebd113fc079ab6cd8b605fc5c50e26
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2025-12-13 12:44:34 -0300

    Como Set testa igualdade

--- curso_programacao/src/curso_programacao/Main.java
+++ curso_programacao/src/curso_programacao/Main.java
@@ -1,36 +1,21 @@
 package curso_programacao;
 
+import model.entities.Product;
+
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
 
-        Set<String> set = new HashSet<>();
-        Set<Integer> a = new TreeSet<>(Arrays.asList(0,2,4,5,6,8,10));
-        Set<Integer> b = new TreeSet<>(Arrays.asList(5,6,7,8,9,10));
+        Set<Product> set = new HashSet<>();
 
-        set.add("TV");
-        set.add("Notebook");
-        set.add("Tablet");
+        set.add(new Product("TV", 900.0));
+        set.add(new Product("Notebook", 1200.0));
+        set.add(new Product("Tablet", 400.0));
 
-        System.out.println(set.contains("Notebook"));
+        Product prod = new Product("Notebook", 1200.0);
 
-        //union
-        Set<Integer> c = new TreeSet<>(a);
-        c.addAll(b);
-        System.out.println(c);
-
-        //intersection
-        Set<Integer> d = new TreeSet<>(a);
-        d.retainAll(b);
-        System.out.println(d);
-
-        //difference
-        Set<Integer> e = new TreeSet<>(a);
-        e.removeAll(b);
-        System.out.println(e);
-
-
+        System.out.println(set.contains(prod));
 
     }
 
