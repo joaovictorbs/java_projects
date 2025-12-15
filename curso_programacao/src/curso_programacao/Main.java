fd2bb1f027e7ecabff80ae6294c7c797412548f1
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2025-12-15 18:58:03 -0300

    Lambda 2

--- curso_programacao/src/curso_programacao/Main.java
+++ curso_programacao/src/curso_programacao/Main.java
@@ -13,14 +13,10 @@ public class Main {
         list.add(new Product("Notebook", 1200.00));
         list.add(new Product("Tablet", 450.00));
 
-        Comparator<Product> comp = new Comparator<Product>() {
-            @Override
-            public int compare(Product p1, Product p2) {
-                return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
-            }
-        };
+        Comparator<Product> comp = (p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
 
-        list.sort(new MyComparator());
+        list.sort(comp);
+
 
         for (Product p : list) {
             System.out.println(p);
