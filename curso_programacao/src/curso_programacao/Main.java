5f97c59188a910d1dc9299384038a94af6286b8f
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2025-12-16 22:18:04 -0300

    Pipeline (demo)

--- curso_programacao/src/curso_programacao/Main.java
+++ curso_programacao/src/curso_programacao/Main.java
@@ -12,17 +12,18 @@ public class Main {
     public static void main(String[] args) {
 
         List<Integer> list = Arrays.asList(3, 4, 5, 10, 7);
-        Stream<Integer> st1 = list.stream();
+
+        Stream<Integer> st1 = list.stream().map(x -> x * 10);
         System.out.println(Arrays.toString(st1.toArray()));
 
-        Stream<String> st2 = Stream.of("Maria", "Alex", "Bob");
-        System.out.println(Arrays.toString(st2.toArray()));
+        int sum = list.stream().reduce(0, (x, y) -> x + y);
+        System.out.println("Sum = " + sum);
 
-        Stream<Integer> st3 = Stream.iterate(0 , x -> x + 2);
-        System.out.println(Arrays.toString(st3.limit(10).toArray()));
-
-        Stream<Long> st4 = Stream.iterate(new Long[]{ 0L, 1L }, p->new Long[]{ p[1], p[0]+p[1] }).map(p -> p[0]);
-        System.out.println(Arrays.toString(st4.limit(10).toArray()));
+        List<Integer> newList = list.stream()
+                .filter(x -> x % 2 == 0)
+                .map(x -> x * 10)
+                .collect(Collectors.toList());
+        System.out.println(Arrays.toString(newList.toArray()));
 
     }
 
