881aeab43f7dd757644bf53cc18a3477bc55e491
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2026-03-21 14:01:47 -0300

    User update

--- curso_programacao/JavaSpringBootJPA/course/src/main/java/com/educandoweb/course/resources/UserResource.java
+++ curso_programacao/JavaSpringBootJPA/course/src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.educandoweb.course.resources;
 
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +18,6 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.services.UserService;
-import com.sun.org.apache.xerces.internal.util.URI;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -46,9 +47,15 @@ public class UserResource {
 		return ResponseEntity.created(uri).body(obj);
 	}
 	
-	@DeleteMapping
+	@DeleteMapping(value = "/{id}")
 	public ResponseEntity<Void> delete(@PathVariable Long id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj) {
+		obj = service.update(id, obj);
+		return ResponseEntity.ok().body(obj);
 	}
 }
