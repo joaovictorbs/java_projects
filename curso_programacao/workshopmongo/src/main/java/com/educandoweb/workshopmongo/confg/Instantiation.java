b46d339cff3355b07181a39eeeed75febc492a51
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2026-03-22 12:07:05 -0300

    Usando padrao DTO para retornar usuarios

--- curso_programacao/workshopmongo/src/main/java/com/educandoweb/workshopmongo/confg/Instantiation.java
+++ curso_programacao/workshopmongo/src/main/java/com/educandoweb/workshopmongo/confg/Instantiation.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
 
 import com.educandoweb.workshopmongo.domain.User;
 import com.educandoweb.workshopmongo.repository.UserRepository;
